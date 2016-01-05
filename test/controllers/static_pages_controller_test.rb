1806007ce74570c5120b134065fe7e25e8762ed8
Michael Washburn <washburn.michael@gmail.com>
2016-01-05 14:57:10 -0800

    updated static pages with dry code for titles to pull from layouts/application.html.erb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | A place for people who love the outdoors, and their dogs"
   end
 
   test "should get profile" do
     get :profile
     assert_response :success
+    assert_select "title", "Profile | A place for people who love the outdoors, and their dogs"
   end
 
   test "should get explore" do
     get :explore
     assert_response :success
+    assert_select "title", "Explore | A place for people who love the outdoors, and their dogs"
   end
 
   test "should get results" do
     get :results
     assert_response :success
+    assert_select "title", "Results | A place for people who love the outdoors, and their dogs"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | A place for people who love the outdoors, and their dogs"
   end
 
 end
