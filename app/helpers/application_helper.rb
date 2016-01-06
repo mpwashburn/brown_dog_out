3c6668bf371a59cbd33f665d529713550d4a0c04
Michael Washburn <washburn.michael@gmail.com>
2016-01-05 17:17:37 -0800

    filled in much of the layout for nav bar links, classes and ids for styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-
+  # Returns the full title on a per-page basis. In other words, If a page was missing a title, then this helper still renders the base_title, which is defined in this helper
   def full_title (page_title = '')
     base_title = "A place for people who love the outdoors, and their dogs"
     if page_title.empty?
