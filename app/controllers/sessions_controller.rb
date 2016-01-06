c458b5f8965ba2500a9bb4a85bb74b3133506537
Michael Washburn <washburn.michael@gmail.com>
2016-01-06 15:49:04 -0800

    Change layout links for login-log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to user
-      # Log the user in and redirect to the user's show pager.
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
