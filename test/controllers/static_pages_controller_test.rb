54db63844cd5745cccc07f117fbbe77a3e092a5e
Michael Washburn <washburn.michael@gmail.com>
2016-01-05 16:25:50 -0800

    changed helper and updated home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | A place for people who love the outdoors, and their dogs"
+    assert_select "title", "A place for people who love the outdoors, and their dogs"
   end
 
   test "should get profile" do
